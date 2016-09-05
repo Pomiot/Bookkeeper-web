fdf67a11b89a85a481e3122b3be43fb6b2bcaaa4
Tomasz <pomiot@gmail.com>
2016-09-05 21:37:06 +0200

    Added absolutely basic model

---
+++ src/main/java/com/pomiot/bookkeeper/model/Author.java
@@ -0,0 +1,45 @@
+package com.pomiot.bookkeeper.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
