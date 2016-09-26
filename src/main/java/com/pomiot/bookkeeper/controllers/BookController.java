c78703504767340c27cb154b46c3f12ef98000e2
Tomasz <pomiot@gmail.com>
2016-09-26 13:11:30 +0200

    - changed controllers to return ResponseEntities - added update book endpoint

--- src/main/java/com/pomiot/bookkeeper/controllers/BookController.java
+++ src/main/java/com/pomiot/bookkeeper/controllers/BookController.java
@@ -3,7 +3,10 @@ package com.pomiot.bookkeeper.controllers;
 import com.pomiot.bookkeeper.model.Book;
 import com.pomiot.bookkeeper.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 public class BookController {
@@ -12,23 +15,28 @@ public class BookController {
     private BookService bookService;
 
     @RequestMapping(path = "/book", method = RequestMethod.GET)
-    public Iterable<Book> listAllBooks(){
-        return bookService.getAllBooks();
+    public ResponseEntity<Iterable<Book>> listAllBooks(){
+        return ResponseEntity.ok().body(bookService.getAllBooks());
     }
 
     @RequestMapping(path = "/book/{id}", method = RequestMethod.GET)
-    public Book getBook(@PathVariable Long id){
-        return bookService.getBookById(id);
+    public ResponseEntity<Book> getBook(@PathVariable long id){
+        return ResponseEntity.ok().body(bookService.getBookById(id));
     }
 
     @RequestMapping(path = "/book/", method = RequestMethod.POST)
-    public void addBook(@RequestBody Book book){
-        bookService.addBook(book);
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        return ResponseEntity.created(URI.create("")).body(bookService.addBook(book));
     }
 
     @RequestMapping(path = "/book/{id}", method = RequestMethod.DELETE)
-    public void deleteBook(@PathVariable Long id){
+    public ResponseEntity.HeadersBuilder<?> deleteBook(@PathVariable long id){
         bookService.deleteBook(id);
+        return ResponseEntity.noContent();
     }
 
+    @RequestMapping(path = "/book/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Book> updateBook(@PathVariable long id, @RequestBody Book book){
+        return ResponseEntity.ok().body(bookService.updateBook(id, book));
+    }
 }
--- src/main/java/com/pomiot/bookkeeper/services/BookService.java
+++ src/main/java/com/pomiot/bookkeeper/services/BookService.java
@@ -1,15 +1,20 @@
 package com.pomiot.bookkeeper.services;
 
 import com.pomiot.bookkeeper.model.Book;
+import com.pomiot.bookkeeper.repositories.AuthorRepository;
 import com.pomiot.bookkeeper.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
 
     public Iterable<Book> getAllBooks(){
         return bookRepository.findAll();
@@ -19,11 +24,23 @@ public class BookService {
         return bookRepository.findOne(id);
     }
 
-    public void addBook(Book book){
-        bookRepository.save(book);
+    public Book addBook(Book book){
+        return bookRepository.save(book);
     }
 
     public void deleteBook(Long id){
         bookRepository.delete(id);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        Book bookFromRepository = bookRepository.findOne(id);
+        bookFromRepository.setTitle(book.getTitle());
+        bookFromRepository.setAuthor(book.getAuthor());
+        bookFromRepository.setDescription(book.getDescription());
+
+        //this is potentially extremely bad
+        authorRepository.save(book.getAuthor());
+
+        return bookRepository.save(bookFromRepository);
     }
 }
