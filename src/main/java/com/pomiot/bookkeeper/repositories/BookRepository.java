fec5c8b666ee209ffb39b305e14d414a89ef2f9f
Tomasz <pomiot@gmail.com>
2016-09-28 07:26:28 +0200

    - removed author repo - changed book repo to spring data rest repo - added home controller - added main page template (wip) - added dependencies to pom

--- src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
+++ src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.pomiot.bookkeeper.repositories;
 
 import com.pomiot.bookkeeper.model.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book,Long> {
 }
--- src/main/java/com/pomiot/bookkeeper/services/BookService.java
+++ src/main/java/com/pomiot/bookkeeper/services/BookService.java
@@ -1,7 +1,6 @@
 package com.pomiot.bookkeeper.services;
 
 import com.pomiot.bookkeeper.model.Book;
-import com.pomiot.bookkeeper.repositories.AuthorRepository;
 import com.pomiot.bookkeeper.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +11,6 @@ public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
-
-    @Autowired
-    private AuthorRepository authorRepository;
 
     public Iterable<Book> getAllBooks(){
         return bookRepository.findAll();
@@ -37,9 +33,6 @@ public class BookService {
         bookFromRepository.setTitle(book.getTitle());
         bookFromRepository.setAuthor(book.getAuthor());
         bookFromRepository.setDescription(book.getDescription());
-
-        //this is potentially extremely bad
-        authorRepository.save(book.getAuthor());
 
         return bookRepository.save(bookFromRepository);
     }
