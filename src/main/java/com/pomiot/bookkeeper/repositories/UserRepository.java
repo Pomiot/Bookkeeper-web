fa7256532451a4653a4ca69fa9143ca510eb52de
Tomasz <pomiot@gmail.com>
2016-10-10 16:10:24 +0200

    - user repo is no longer publicly exposed

--- src/main/java/com/pomiot/bookkeeper/repositories/UserRepository.java
+++ src/main/java/com/pomiot/bookkeeper/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.pomiot.bookkeeper.repositories;
 
 import com.pomiot.bookkeeper.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long>{
 
     User findByUsername(String username);
