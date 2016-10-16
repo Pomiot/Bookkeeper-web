9ddf5663239d1175c0bde3d2ab918c45c74340a0
Tomasz <pomiot@gmail.com>
2016-10-16 15:19:23 +0200

    - application now uses password hashes - cleanup

--- src/main/java/com/pomiot/bookkeeper/config/SecurityConfig.java
+++ src/main/java/com/pomiot/bookkeeper/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pomiot.bookkeeper.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(userDetailsService);
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/pomiot/bookkeeper/model/User.java
+++ src/main/java/com/pomiot/bookkeeper/model/User.java
@@ -1,5 +1,6 @@
 package com.pomiot.bookkeeper.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,29 +10,27 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter @Setter
 public class User {
 
-    @Getter @Setter
     @Id
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Getter @Setter
+    @JsonIgnore
     @Column(name = "password", nullable = false)
     @NotNull
     private String password;
 
-    @Getter @Setter
+    @JsonIgnore
     @Column(name = "enabled", nullable = false)
     @NotNull
     private Boolean enabled;
 
-    @Getter @Setter
     @Column(name = "role", nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Getter @Setter
     @OneToMany(mappedBy = "owner")
     List<Book> booksOwned;
 }
