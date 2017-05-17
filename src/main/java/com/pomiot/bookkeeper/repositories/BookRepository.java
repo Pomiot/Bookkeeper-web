b579afd4e720a333a23a98e0da913fc29381996e
Tomasz <pomiot@gmail.com>
2017-05-17 22:17:55 +0200

    - added @CrossOrigin on repo for temporary free access

--- src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
+++ src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
@@ -4,11 +4,15 @@ import com.pomiot.bookkeeper.model.Book;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+@CrossOrigin(allowedHeaders = "*", origins = "*",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.PATCH})
 @RepositoryRestResource
-public interface BookRepository extends PagingAndSortingRepository<Book,Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
     List<Book> findBookByTitleLikeIgnoreCase(@Param("title") String title);
 
--- src/test/java/com/pomiot/bookkeeper/BookkeeperApplicationTests.java
+++ src/test/java/com/pomiot/bookkeeper/BookkeeperApplicationTests.java
@@ -3,14 +3,59 @@ package com.pomiot.bookkeeper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.SystemProfileValueSource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.persistence.criteria.CriteriaBuilder;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class BookkeeperApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		Printer printer = new Printer();
+
+		printer.registerObserver(new PrinterObserverImpl());
+
+		printer.setNumber(10000);
+	}
+
+	class Printer {
+
+		List<PrinterObserver> observers = new ArrayList<>();
+
+		Integer number = 0;
+
+		public void registerObserver(PrinterObserver observer){
+			observers.add(observer);
+		}
+
+		public void notifyObservers(){
+			observers.forEach(o -> o.update(this));
+		}
+
+		public void setNumber(Integer integer){
+			this.number = integer;
+			notifyObservers();
+		}
+	}
+
+	interface PrinterObserver {
+		void update(Printer printer);
+	}
+
+	class PrinterObserverImpl implements PrinterObserver {
+
+		@Override
+		public void update(Printer printer) {
+			System.out.println("Printer got changed! New number is: " + printer.number);
+		}
 	}
 
 }
