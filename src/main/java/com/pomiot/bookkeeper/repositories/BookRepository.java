c7dd478f6b76417b9a31ce062f74031f9f76ab31
Tomasz Grzesiak <tomasz.grzesiak@capgemini.com>
2017-05-18 14:00:38 +0200

    - changed repo type to CrudRepository

--- src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
+++ src/main/java/com/pomiot/bookkeeper/repositories/BookRepository.java
@@ -1,16 +1,14 @@
 package com.pomiot.bookkeeper.repositories;
 
 import com.pomiot.bookkeeper.model.Book;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @RepositoryRestResource
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
     List<Book> findBookByTitleLikeIgnoreCase(@Param("title") String title);
 
